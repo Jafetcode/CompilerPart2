8b428f5e95c093e31b9aa2d453b72fd1a3a11285
Jacob Luytjes <jluytjes01@aurora.edu>
2025-03-15 18:55:13 -0500

    Update Basic Code so far
    This is broken and not ready with code still being tested and trying stuff, but so far so good.

--- CompilerPart1/LexAnalyzer.cpp
+++ CompilerPart1/LexAnalyzer.cpp
@@ -1,4 +1,3 @@
-
 #include "LexAnalyzer.h"
 // pre: parameter refers to open data file consisting of token and
 // lexeme pairs i.e.  s_and and t_begin begin t_int 27
@@ -6,7 +5,13 @@
 // post: tokenmap has been populated - key: lexeme, value: token
 
 LexAnalyzer::LexAnalyzer(istream& infile) {
-
+    string wordPair;
+    while (std::getline(infile, wordPair)) {
+        unsigned int pos = wordPair.find(" ");
+        string token = wordPair.substr(0, pos);
+        string lex = wordPair.substr(pos + 1, wordPair.size());
+        tokenmap.insert(make_pair(lex, token));
+    }
 }
 
 // pre: 1st parameter refers to an open text file that contains source
@@ -18,5 +23,43 @@ LexAnalyzer::LexAnalyzer(istream& infile) {
 // an error message have been written to the output file.
 // A success or fail message has printed to the console.
 void LexAnalyzer::scanFile(istream& infile, ostream& outfile) {
+    string lineOfCode;
+    while (getline(infile, lineOfCode)) {
+        for (int i = 0; i < lineOfCode.size(); ++i) {
+            if (lineOfCode[i] != ' ') {
+                if (lineOfCode[i] >= '0' && lineOfCode[i] <= '9') {
+                    int number = 1;
+                    while (lineOfCode[i+number] >= '0' && lineOfCode[i+number] <= '9') {
 
+                    }
+                    outfile << "t_number : " << lineOfCode.substr(i, number) << endl;
+                }
+                else if (lineOfCode[i] == '"') {
+                    int number = 1;
+                    while (lineOfCode[i+number] != '"') {
+
+                    }
+                    outfile << "t_text : " << lineOfCode.substr(i+1, number-1) << endl;
+                }
+
+                map<string, string>::iterator it = tokenmap.begin();
+                bool found = false;
+                while (it != tokenmap.end() && !found) {
+                    if (it->first.size() <= lineOfCode.size() - i) {
+                        //cout << "Test: " << lineOfCode.substr(i, it->first.size()) << endl;
+                        if (it->first == lineOfCode.substr(i, it->first.size())) {
+                            outfile << it->second << " : " << it->first << endl;
+                            found = true;
+                            i += it->first.size() - 1;
+                        }
+                    }
+                    ++it;
+                }
+
+                if (!found) {
+                    cout << lineOfCode << ": " << i << endl;
+                }
+            }
+        }
+    }
 }
