31135aeb133689662be5b6df36824deba46b79bf
Jacob Luytjes <jluytjes01@aurora.edu>
2025-03-19 13:52:51 -0500

    Update LexAnalyzer.h

--- CompilerPart1/LexAnalyzer.h
+++ CompilerPart1/LexAnalyzer.h
@@ -1,3 +1,6 @@
+// created by Jacob Luytjes, David Gonzalez, Abraham Hernandez,
+// David Llamas, Ethan Gallenbach, Jafet Isidoro.
+
 #ifndef LEXANALYZER_H
 #define LEXANALYZER_H
 // above can also be achieved with #pragma once
@@ -9,38 +12,36 @@
 using namespace std;
 
 class LexAnalyzer {
-    private:
-        // map to hold language's valid lexeme/token pairs
-        map<string, string> tokenmap;
+private:
+    // map to hold language's valid lexeme/token pairs
+    map<string, string> tokenmap;
 
-        // parallel vectors to store valid lexemes and tokens from source code file
-        vector<string> lexemes;
-        vector<string> tokens;
+    // parallel vectors to store valid lexemes and tokens from source code file
+    vector<string> lexemes;
+    vector<string> tokens;
 
-        // other private methods
-        bool validChecker(int start, int end, string lineOfCode);
+    // other private methods
+    bool validChecker(int start, int end, const string& lineOfCode);
+    void processNumber(size_t& i, const std::string& lineOfCode, std::ostream& outfile, bool& error);
+    void processString(size_t& i, const std::string& lineOfCode, std::ostream& outfile, bool& error);
+    void processKeywordOrIdentifier(size_t& i, const std::string& lineOfCode, std::ostream& outfile, bool& error);
+    void processIdentifier(size_t& i, const std::string& lineOfCode, std::ostream& outfile, bool& error, bool& found);
 
-    public:
-        LexAnalyzer(istream& infile);
-        // pre: parameter refers to open data file consisting of token and
-        // lexeme pairs i.e.  s_and and t_begin begin t_int 27
-        // Each pair appears on its own input line.
-        // post: tokenmap has been populated - key: lexeme, value: token
+public:
+    LexAnalyzer(istream& infile);
+    // pre: parameter refers to open data file consisting of token and
+    // lexeme pairs i.e.  s_and and t_begin begin t_int 27
+    // Each pair appears on its own input line.
+    // post: tokenmap has been populated - key: lexeme, value: token
 
-        void scanFile(istream& infile, ostream& outfile);
-        // pre: 1st parameter refers to an open text file that contains source
-        // code in the language, 2nd parameter refers to an open empty output
-        // file
-        // post: If no error, the token and lexeme pairs for the given input
-        // file have been written to the output file (token : lexeme).
-        // If there is an error, the incomplete token/lexeme pairs, as well as
-        // an error message have been written to the output file.
-        // A success or fail message has printed to the console.
-
-        void lexNum(string lineOfCode);
-
-        void lexString(string lineOfCode);
-
-        void lexID(string lineOfCode);
+    void scanFile(istream& infile, ostream& outfile);
+    // pre: 1st parameter refers to an open text file that contains source
+    // code in the language, 2nd parameter refers to an open empty output
+    // file
+    // post: If no error, the token and lexeme pairs for the given input
+    // file have been written to the output file (token : lexeme).
+    // If there is an error, the incomplete token/lexeme pairs, as well as
+    // an error message have been written to the output file.
+    // A success or fail message has printed to the console.
 };
 #endif //LEXANALYZER_H
